90c59221983b9d64394ba6a1e55823e92cdc0373
Evan Yang <quantbit@126.com>
2026-08-17 10:57:26 +0800

    Add TODO field

--- shredder_encryptor_cli/__init__.py
+++ shredder_encryptor_cli/__init__.py
@@ -1,4 +1,5 @@
 """Shredder Encryptor CLI Command Line Tool System"""
 
-__version__ = "2026.8.1-pre5"
+# TODO: Add cli for shredder_encryptor.boom submodule
+__version__ = "2026.8.1-pre6"
 __license__ = "MIT"
